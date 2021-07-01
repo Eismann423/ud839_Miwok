de42e299d36b3bb5037e1787f63e89a3d532b0cc
NikolaiMilburn <nicholasgtmilburn@gmail.com>
2021-06-30 18:27:48 -0600

    added resources

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -20,7 +20,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,38 @@
+package com.example.android.miwok;
+
+public class Word {
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceID = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceID = imageResourceID;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceID() {
+        return  mImageResourceID;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceID != NO_IMAGE_PROVIDED;
+    }
+
+}
+
